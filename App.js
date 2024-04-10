db49064f780289454ac8a46a653d422282daaa50
chiffon <chocolate@chiffoncake.local>
2024-04-10 21:36:01 +0900

    firstCommit

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {WebView} from "react-native-webview";
+
+const youtube = "https://www.youtube.com/";
+const google = "https://www.google.com/";
+const chiffon = "http://chiffoncakes.starfree.jp/Vocaloids/vocalo.html";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{width:"100%", height:"100%"}}>
+        <WebView
+          source = {{uri: chiffon}}
+          onLoad={console.log("loaded")}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,8 +22,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop:20,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222',
     alignItems: 'center',
     justifyContent: 'center',
   },
